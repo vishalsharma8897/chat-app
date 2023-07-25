479f78e82655b5e50be73deebf9436794d2ce77b
vishalsharma8897 <vishalsharma19052001@gmail.com>
2023-07-25 21:13:44 +0530

    final round

--- server/index.js
+++ server/index.js
@@ -1,52 +1,50 @@
-const connectToMongo= require("./db");// imported connectToMongo for connection 
+const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
 const socket = require("socket.io");
 const { v4: uuidv4 } = require('uuid');
 require("dotenv").config();
-const userRoutes= require("./Routes/userRoutes");
-const messagesRoute= require("./Routes/messagesRoute");
-
+const userRoutes = require("./Routes/userRoutes");
+const messagesRoute = require("./Routes/messagesRoute");
 
 const app = express();
 
-
+// Enable CORS for Express app
 app.use(cors());
 
 app.use(express.json());
 
-
 let port = process.env.PORT || 8080;
-connectToMongo();  // connected 
+connectToMongo();
 
-// Availbale routes;
-app.use('/api/auth',userRoutes);    // router object wiil be replaced by require(...) and  wiill act as a middleware function 
-app.use('/api/messages',messagesRoute);    // router object wiil be replaced by require(...) and  wiill act as a middleware function 
-
-
-
-const server = app.listen(port,()=>{
+const server = app.listen(port, () => {
     console.log("Server started at " + port);
-})
+});
 
- const io = socket(server);
+// Enable CORS for Socket.IO
+const io = socket(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
- global.onlineUsers= new Map();
+global.onlineUsers = new Map();
 
- io.on("connection",(socket)=>{
-    global.chatSocket=socket;
-    socket.on("add-user",(userId)=>{
-        onlineUsers.set(userId,socket.id);
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
     });
 
-    socket.on("send-msg",(data)=>{
-        const sendUserSocket= onlineUsers.get(data.to);
-        if(sendUserSocket){
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
             const message = {
                 id: uuidv4(), // Generate a unique ID for the message
                 message: data.message,
-              };
-            socket.to(sendUserSocket).emit("msg-recieve",message);
+            };
+            socket.to(sendUserSocket).emit("msg-recieve", message);
         }
     });
- });
+});

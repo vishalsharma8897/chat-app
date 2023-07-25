08df7e0aafc2478350146bc364d88fe578fb05c7
vishalsharma8897 <vishalsharma19052001@gmail.com>
2023-07-25 17:30:24 +0530

    changed

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:8080"
+export const host = process.env.HOST;
 
 export const registerRoute = `${host}/api/auth/register`                            
 export const loginRoute = `${host}/api/auth/login`                            

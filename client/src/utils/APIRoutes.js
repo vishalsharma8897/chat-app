4001f71708121c15ac4971b9b50f805b6a6a0b76
vishalsharma8897 <vishalsharma19052001@gmail.com>
2023-07-26 16:49:13 +0530

    done

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-// export const host =  "https://chat-back-gf66.onrender.com" ;
-export const host = "http://localhost:8080"
+export const host =  "https://chat-back-gf66.onrender.com" ;
+
 
 export const registerRoute = `${host}/api/auth/register`                            
 export const loginRoute = `${host}/api/auth/login`                            
